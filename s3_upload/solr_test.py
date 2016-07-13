c2c118ed26a5d772e4448d66c2d0900b805f6474
Vlad <vlad@v-lad.org>
2016-07-13 01:13:12 -0400

    Add field configuration script

---
+++ s3_upload/solr_test.py
@@ -0,0 +1,18 @@
+import pytest
+from solr import Solr
+
+
+@pytest.fixture
+def solr():
+    from solr import Solr
+    return Solr('http://52.90.109.169:8983', 'oadata_3_shard1_replica1')
+
+
+def test_we_can_poke_solr(solr):
+    rv = solr.send_request('select?wt=json', '{}')
+    assert rv['response']['numFound'] == 0
+
+
+def test_we_can_add_field(solr):
+    rv = solr.add_field('s3_url', type="string", multi_valued=False)
+    assert rv == 0
